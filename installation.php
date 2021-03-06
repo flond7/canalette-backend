5ddb3f0aa63630a2df0a7516c4097729e9d889aa
flond7 <elisa.utilivenza@gmail.com>
2021-03-06 19:47:19 +0000

    updated db structure

--- installation.php
+++ installation.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "";
 $password = "";
-$dbName = "testDB";
+$dbName = "canalette";
 
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
@@ -50,8 +50,8 @@ if (mysqli_query($link, $tabTwo)) {
   echo "ERROR: Could not execute $tabTwo. " . mysqli_error($link);
 }
 
-// DRAINCHANNEL table
-$tabThree = "CREATE TABLE IF NOT EXISTS drainChannel (
+// DRAINCHANNELS table
+$tabThree = "CREATE TABLE IF NOT EXISTS drainChannels (
   id_drain INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   num VARCHAR(30) NOT NULL,
   street VARCHAR(70),
@@ -67,31 +67,27 @@ if (mysqli_query($link, $tabThree)) {
 // RELATIONAL table
 $tabFour = "CREATE TABLE IF NOT EXISTS relational (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  num VARCHAR(30) NOT NULL,
   amount DOUBLE(40,2) NOT NULL,
   paid BOOLEAN NOT NULL,
   id_user INT NOT NULL,
   id_drain INT NOT NULL,
-  id_year INT NOT NULL,
-  FOREIGN KEY (id_user) REFERENCES users(id_user) ON DELETE CASCADE,
-  FOREIGN KEY (id_drain) REFERENCES drainChannel(id_drain) ON DELETE CASCADE,
-  FOREIGN KEY (id_year) REFERENCES taxes(id_year) ON DELETE CASCADE
+  id_year INT NOT NULL
   )";
 if (mysqli_query($link, $tabFour)) {
   echo "Table RELATIONAL created successfully. ";
 } else {
   echo "ERROR: Could not execute $tabFour. " . mysqli_error($link);
 }
-/* 
+
 // Add test initial data 
 $insertOne = "INSERT INTO users (id_user, first_name, last_name, cf, email, tel, category) VALUES
   (0, 'Elisa', 'Pessa', 'PSSLSE1234567890', 'elisa@mail.it', '0123456789', 'citizen'),
-  (1, 'Eli', 'Pessa', 'PSSLSE1234567890', 'elisa@mail.it', '0123456789', 'citizen'),
+  (1, 'Eli', 'Pes', 'PSSLS4567890', 'elisa@mail.it', '0123456789', 'citizen'),
   (2, 'E', 'Pessa', 'PSSLSE1234567890', 'elisa@mail.it', '0123456789', 'citizen')";
 if (mysqli_query($link, $insertOne)) {
-  echo "Table inserted data successfully.";
+  echo "USER data inserted successfully. ";
 } else {
-  echo "ERROR: Could not execute $insertOne. " . mysqli_error($link);
+  echo "ERROR: Could not insert in USERS " . mysqli_error($link);
 }  
 
 // Add test initial data 
@@ -100,22 +96,34 @@ $insertTwo = "INSERT INTO taxes (id_year, taxCitizen, taxBusiness) VALUES
   (1995, 200, 300),
   (2020, 500, 800)";
 if (mysqli_query($link, $insertTwo)) {
-  echo "Table inserted data successfully.";
+  echo "USER data inserted successfully. ";
 } else {
-  echo "ERROR: Could not execute $insertTwo. " . mysqli_error($link);
+  echo "ERROR: Could not insert in TAXES " . mysqli_error($link);
 }
 
 // Add test initial data 
-$insertThree = "INSERT INTO drainChannel (id_drain, num, street, fogl, map) VALUES
+$insertThree = "INSERT INTO drainChannels (id_drain, num, street, fogl, map) VALUES
   (0, '143', 'Via Roma 1', '12', '5'),
   (1, '1', 'Via Roma 1', '67', '77'),
-  (2, '273', 'Via Roma 1', '2', '53'),";
+  (2, '273', 'Via Roma 1', '2', '53')";
 if (mysqli_query($link, $insertThree)) {
   echo "Table inserted data successfully.";
 } else {
-  echo "ERROR: Could not execute $insertThree. " . mysqli_error($link);
+  echo "ERROR: Could not insert in DRAINCHANNEL " . mysqli_error($link);
 }
- */
+ 
+
+ // Add test initial data 
+$insertThree = "INSERT INTO relational (id, amount, paid, id_user, id_drain, id_year) VALUES
+(0, 1050, true, 1, 1, 1994),
+(1, 950, true, 1, 1, 1995),
+(2, 1050, true, 2, 2, 1994),
+(3, 1050, true, 0, 2, 1995)";
+if (mysqli_query($link, $insertThree)) {
+echo "Table inserted data successfully.";
+} else {
+echo "ERROR: Could not insert in RELATIONAL " . mysqli_error($link);
+}
 // Close connection
 mysqli_close($link);
 ?>
